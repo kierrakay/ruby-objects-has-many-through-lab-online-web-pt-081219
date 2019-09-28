8d56726a5e148a740d9a34729254f6d20d39a963
Kierra M <k.merriman32@hotmail.com>
2019-09-28 02:28:15 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,4 +15,20 @@ class Patient
   def self.all 
     @@all 
   end 
+  
+   def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
+  end
 end
