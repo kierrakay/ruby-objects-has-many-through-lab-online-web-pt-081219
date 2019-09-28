41ea0dcd24cf53c923832ab7ff15401a6ba8945f
Kierra M <k.merriman32@hotmail.com>
2019-09-28 01:55:44 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,19 @@
 class Doctor
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all  
+  end
   
 end
+
