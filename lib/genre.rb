2aa534fe2d4de2be1c3aaf42ebc1f72d32440c5b
Kierra M <k.merriman32@hotmail.com>
2019-09-28 01:25:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,2 +1,22 @@
 class Genre 
-end 
+  
+  attr_accessor :name, :artist 
+  
+  @@all = [] 
+  
+  def initialize(name) 
+    @name = name 
+    @artist = artist 
+    save
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+end
+  
